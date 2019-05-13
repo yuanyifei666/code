9269cc75b6280c2b7e2262af5d0da9bdda281996
yuan <yuan@qq.com>
2019-05-13 08:52:32 +0800

    5月12号

--- maizuo/modules/login/login.js
+++ maizuo/modules/login/login.js
@@ -3,5 +3,5 @@ let loginCallbackBtn = document.querySelector('.callbackImg');
 
 //绑定点击事件,实现历史回退
 loginCallbackBtn.onclick = function(){
-    window.location.href = '/code/maizuo/main.html#center';
+    window.history.go(-1);
 }
--- maizuo/modules/preference/preference.js
+++ maizuo/modules/preference/preference.js
@@ -3,11 +3,15 @@ define(['art','jquery','text!preference.html'],(art ,$, oTemp) => {
 
     function preferenceFn(){
         //获取数据
-        let page = {name: '特惠'}
+        let preferenceData = [
+            'https://image.watsons.com.cn//upload/19b50d68.jpg',
+            'https://image.watsons.com.cn//upload/5823e432.jpg',
+            'https://image.watsons.com.cn//upload/51d6eab5.jpg',
+            'https://image.watsons.com.cn//upload/6127d10a.jpg']
         //填充数据
-        let html = art.render(oTemp,page);
+        let html = art.render(oTemp,preferenceData);
 
-        console.log(html);
+        // console.log(html);
         //数据的渲染
         $('#content-view').html(html);
     }
